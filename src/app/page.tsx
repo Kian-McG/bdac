a9126d9a132672b8cb0f6582b95793b0a7d83eeb
Kian <136632574+Kian-McG@users.noreply.github.com>
2025-01-12 13:43:03 +0000

    remove / imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 
-import AboutUs from "@/components/AboutUs";
-import Activities from "@/components/Activities";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import News from "@/components/News";
+// import AboutUs from "@/components/AboutUs";
+// import Activities from "@/components/Activities";
+// import Footer from "@/components/Footer";
+// import Hero from "@/components/Hero";
+// import News from "@/components/News";
 
 
 
 const Home = () => {
   return (
     <main className="">
-      <Hero />
+      {/* <Hero />
       <AboutUs />
       <Activities />
       <News />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 };
